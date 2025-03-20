760ac620d684caa94a5271d4e1d886d6c37997cc
prs2002 <praneetsuyal+github@gmail.com>
2025-03-21 01:47:16 +0530

    added dashboard components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return(
     <div>
       <SignedOut>
-        <SignInButton mode="modal"/>
-        <SignUpButton mode="modal"/>
+        <SignInButton mode="modal">
+          <Button>Sign in</Button>
+        </SignInButton>
+        <SignUpButton mode="modal">
+          <Button variant={"secondary"} >Sign Up</Button>
+        </SignUpButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+      <ModeToggle></ModeToggle>
     </div>
   );
 }
